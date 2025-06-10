b1d0787c1fb1980f677b74dd377db5144b445285
Massine <massineabgar@hotmail.com>
2025-06-10 20:59:01 +0200

    ArticleModele ok

--- stagiaires/Massine/model/ArticleModel.php
+++ stagiaires/Massine/model/ArticleModel.php
@@ -6,6 +6,54 @@ function addArticle(PDO $conn, array $datas,int $iduser): bool
     $sql = "INSERT INTO `article` (`title`,`articletext`,`articledatecreated`, `articlepublished`,`user_iduser`) VALUES (?,?,?,?,?);";
     $query = $conn->prepare($sql);
     
+    if(isset($datas['articlePublished'])){
+        $isPublished = 1;
+        $datePublished = $datas['datePublished'];
+
+        if(empty($datePublished)){
+            $datePublished = date("Y-m-d H:i:s");
+        }else{
+            $datetime = strtotime($datePublished);
+            $datePublished = date("Y-m-s H:i:s",$datetime);
+        }
+    }else{
+        $isPublished = 0;
+        $datePublished = null;
+    }
+
+    $title = htmlspecialchars(strip_tags(trim($datas['title'])));
+    $text = htmlspecialchars(strip_tags(trim($datas['text'])));
+
+    if(empty($title)||empty($text)) return false;
+
+    try{
+        $query->execute([$title,$text,$datePublished,$isPublished,$iduser]);
+        return true;
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
 
 }
 
+function getArticlePublished(PDO $conn): array
+
+{
+    $sql = "
+            SELECT a.`idarticle`, a.`title`, LEFT(a.`articletext`,290) AS article_text , a.`articledatecreated`, a.`articledatepublished`,a.`articlepublished`,
+        u.login, u.username
+        FROM article a
+        INNER JOIN `user` u
+        	ON u.iduser=a.user_iduser
+        WHERE a.`articlepublished`=1
+        ORDER BY a.`articledatepublished` DESC;
+    ";
+
+    try{
+        $query = $conn->query($sql);
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+}
